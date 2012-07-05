83cdf12ceb71f4cedb5dab1fcde96fd95298d189
kenneth koontz <kenneth.koontz@gmail.com>
2012-07-04 23:57:01 -0700

    json isn't doubly stringified

--- actions.js
+++ actions.js
@@ -16,6 +16,9 @@ Action.prototype.redirect = function(url) {
 }
 
 Action.prototype.json = function (json) {
+    if (typeof json === 'object') {
+        json = JSON.stringify(json);
+    }
     this.response.writeHead(200, {"Content-Type": "application/json"});
     this.response.write(JSON.stringify(json));
     this.response.end();
